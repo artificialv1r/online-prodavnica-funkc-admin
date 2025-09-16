e8fb440fb470fa099347b03492b94de447defa0b
stefangospic <stefan.gospic.1@gmail.com>
2025-09-16 14:29:46 +0200

    Dodata funkcija za prikaz detalja o artiklu ispod tabele, detalji o artiklu se sklanjaju kad se klikne na drugi artikal.

--- app.js
+++ app.js
@@ -25,17 +25,44 @@ function createArticleRows() {
     tr.appendChild(naziv);
     tr.appendChild(cena);
     table.appendChild(tr);
+
+    tr.addEventListener("click", function () {
+      displayArticleDetails(artikli[i]);
+      document.querySelector("#articleDetails").style.border =
+        "1px solid black";
+    });
   }
 }
 
 function initializeArticels() {
   artikli = [
-    new Artikal("Monitor", 165, ""),
-    new Artikal("Tv", 650, ""),
-    new Artikal("Mis", 20, ""),
+    new Artikal("Monitor", 165, "SAMSUNG S5 34 100Hz"),
+    new Artikal("Tv", 650, "Samsung TV 65 4K"),
+    new Artikal("Mis", 20, "Razer Mouse Viper 8KHz"),
   ];
 
   createArticleRows();
+}
+
+function displayArticleDetails(artikal) {
+  let p = document.createElement("p");
+
+  p.innerHTML =
+    "Naziv: " +
+    artikal.naziv +
+    "<br>" +
+    "Cena: " +
+    artikal.cena +
+    "$" +
+    "<br>" +
+    "Opis: " +
+    artikal.opis;
+
+  let detalji = document.querySelector("#articleDetails");
+  if (detalji.firstChild) {
+    detalji.firstChild.remove();
+  }
+  detalji.appendChild(p);
 }
 
 document.addEventListener("DOMContentLoaded", initializeArticels);
