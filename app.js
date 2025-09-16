0a210606bfc23f9e63768a69282cb9b3930642f0
stefangospic <stefan.gospic.1@gmail.com>
2025-09-16 14:00:16 +0200

    Definisana klasa za artikal, definisan JS kod koji ce HTML tabelu popuniti sa nizom proizvoda.

--- app.js
+++ app.js
@@ -7,3 +7,35 @@ class Artikal {
 }
 
 let artikli = [];
+
+function createArticleRows() {
+  let table = document.querySelector("#articles-body");
+
+  for (let i = 0; i < artikli.length; i++) {
+    let tr = document.createElement("tr");
+    let br = document.createElement("td");
+    let naziv = document.createElement("td");
+    let cena = document.createElement("td");
+
+    br.textContent = i + 1;
+    naziv.textContent = artikli[i].naziv;
+    cena.textContent = artikli[i].cena;
+
+    tr.appendChild(br);
+    tr.appendChild(naziv);
+    tr.appendChild(cena);
+    table.appendChild(tr);
+  }
+}
+
+function initializeArticels() {
+  artikli = [
+    new Artikal("Monitor", 165, ""),
+    new Artikal("Tv", 650, ""),
+    new Artikal("Mis", 20, ""),
+  ];
+
+  createArticleRows();
+}
+
+document.addEventListener("DOMContentLoaded", initializeArticels);
